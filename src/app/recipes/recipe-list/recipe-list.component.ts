4df23b5976140c639f4f7134fcdc930ffd0d38a4
Benoit Martins <benoit.martins@outlook.com>
2019-12-05 10:13:45 +0100

    Create recipe-list  component :- Mock a recipe item- Add components to their parents template- Add styles to improve display

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  public recipes: Recipe[] = [
+    new Recipe('Grunt pancake',
+      'Top rank pancake for sunday breakfast',
+      'https://cdn.pixabay.com/photo/2018/11/05/12/42/food-3796017_960_720.jpg'
+    )
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
