68d5d9c7e9a127de2813a35d71de09ce421ef019
Benoit Martins <benoit.martins@outlook.com>
2019-12-07 23:41:05 +0100

    Add recipe selection :
    - Create event pass to parent component to send selected recipe
    - Update template to use event and props
    - Update detail template to display selected recipe detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() listSelectedRecipe = new EventEmitter<Recipe>();
+  
   public recipes: Recipe[] = [
     new Recipe('Grunt pancake',
       'Top rank pancake for sunday breakfast',
@@ -25,6 +27,11 @@ export class RecipeListComponent implements OnInit {
       'https://cdn.pixabay.com/photo/2018/11/05/12/42/food-3796017_960_720.jpg'
     ),
   ];
+
+  onRecipeSelected(recipe: Recipe) {
+    console.log("recipe-list: ", recipe)
+    this.listSelectedRecipe.emit(recipe);
+  }
 
   constructor() { }
 
